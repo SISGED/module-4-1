003851fcda7f91144640aefa575c216b62e53423
SISGED <spicin1995@mail.ru>
2024-08-14 13:58:46 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def div(first, second):
+    if second != 0:
+        i = first / second
+        return i
+    else:
+        print('Ошибка')
---
+++ true_math.py
@@ -0,0 +1,8 @@
+from math import inf
+
+def div(first, second):
+    if second != 0:
+        j = first / second
+        return j
+    else:
+        return inf
